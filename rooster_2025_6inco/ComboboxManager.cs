442b144969e827d8163f8a8df3ba1832ff73009c
Lennard Laurenz Vanhoutte <lennard.vanhoutte@leerling.go-ao.be>
2025-10-06 13:05:17 +0200

    ComboboxManager.cs

--- rooster_2025_6inco/ComboboxManager.cs
+++ rooster_2025_6inco/ComboboxManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace rooster_2025_6inco
 {
     internal class ComboboxManager
     {
-
+       public ComboboxManager(Panel layout, CsvReader reader)
+       {
+            List<Leerkracht> leerkrachten = reader.getLeerkrachten();
+            List<Klas> klassen = reader.getKlassen();
+            List<Lokaal>lokalen = reader.getLokalen();
+       }
     }
 }
