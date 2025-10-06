8e080e0b5ee9ffee4e5bb87173b2951b0038e664
Thomas Lerouge <thomas.lerouge@leerling.go-ao.be>
2025-10-06 13:36:17 +0200

    for lus

--- rooster_2025_6inco/RoosterPanelManager.cs
+++ rooster_2025_6inco/RoosterPanelManager.cs
@@ -9,9 +9,17 @@ namespace rooster_2025_6inco
 {
     internal class RoosterPanelManager
     {
+        lesuur[,] les = new lesuur[5, 7];
         public RoosterPanelManager(Panel panel, ComboboxManager comboboxManager) 
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 7; j++)
+                {
+                    Panel lesuurPanel = new Panel();
+                    //les[i, j] = new lesuur()
+                }
+            }
         }
     }
 }
