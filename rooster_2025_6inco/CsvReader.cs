b0a7f74776eca9da46343bb98476ed282872aad4
Kiril Pushnoy <kiril.pushnoy@leerling.go-ao.be>
2025-10-06 12:57:52 +0200

    added CsvReader constructor

--- rooster_2025_6inco/CsvReader.cs
+++ rooster_2025_6inco/CsvReader.cs
@@ -8,5 +8,24 @@ namespace rooster_2025_6inco
 {
     internal class CsvReader
     {
+
+        public CsvReader(string filePath)
+        {
+
+        }
+        public List<Klas> getKlassen()
+        {
+            return new List<Klas>();
+        }
+
+        public List<Leerkracht> getLeerkrachten()
+        {
+            return new List<Leerkracht>();
+        }
+
+        public List<Lokaal> getLokalen()
+        {
+            return new List<Lokaal>();
+        }
     }
 }
