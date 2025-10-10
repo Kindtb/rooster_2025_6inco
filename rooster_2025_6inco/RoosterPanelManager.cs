427facb4a35ebee4b45431e9e9b7e1ca61faee56
Thomas Lerouge <thomas.lerouge@leerling.go-ao.be>
2025-10-10 11:53:22 +0200

    labels tonen

--- rooster_2025_6inco/RoosterPanelManager.cs
+++ rooster_2025_6inco/RoosterPanelManager.cs
@@ -4,33 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace rooster_2025_6inco
 {
     internal class RoosterPanelManager
     {
-        Lesuur[,] les = new Lesuur[5, 7];
-        public RoosterPanelManager(Panel panel, ComboboxManager comboboxManager) 
+        Lesuur[,] lesLeerkracht = new Lesuur[5, 7];
+        public RoosterPanelManager(Panel panel, ComboboxManager comboboxManager)
         {
             for (int i = 0; i < 5; i++)
             {
-                for(int j = 0; j < 7; j++)
+                for (int j = 0; j < 7; j++)
                 {
+
                     Panel lesuurPanel = new Panel();
-                    //les[i, j] = new lesuur()
+                    lesuurPanel.Location = new Point(i * 50, j * 20);
+                    lesuurPanel.Size = new Size(50, 20);
+                    lesuurPanel.BorderStyle = BorderStyle.FixedSingle;
+                    panel.Controls.Add(lesuurPanel);
+                    //lesLeerkracht[i, j] = new Lesuur(lesuurPanel, comboboxManager);
                 }
+
+
+                comboboxManager.leerkrachtChanged += (sender, e) =>
+                {
+
+                    string leerkracht = e;
+                    // toon de rooster van leerkracht
+                };
             }
 
-            comboboxManager.leerkrachtChanged += (sender, e) =>
-            {
 
-                string leerkracht = e;
-                // toon de rooster van leerkracht
-            };
         }
 
-        
-    }
 
-    
+    }
 }
